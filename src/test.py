ebd0e54958dac434e77d9995d21930d154a7082f
Pavel Sterin <codeatstake@googlemail.com>
2017-02-18 18:13:31 +0100

    add talking to rabbits

--- src/test.py
+++ src/test.py
@@ -16,9 +16,9 @@ import game.action as action
 
 class Balkony(Room):
     description = (
-            'You saw just a regular {{ object | obj }}.'
+        'You saw just a regular {{ object | obj }}.'
     )
-    
+
     def name(self):
         return 'balkony'
 
@@ -26,17 +26,17 @@ class Balkony(Room):
 class Rabbit(Object):
     def __init__(self, *args, name=None, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
         self.named = name
-        
+
         self.state['rabbit'] = 'hungry'
-        
+
         self.touch_replies = Reply([
             '{{ object | obj }} was warm and fluffy to the'
             ' {{ verb | inf }}.'
         ])
         self.actions['touch'] = self.touch_replies
-                                      
+
         self.look_hungry_replies = Reply([
             'You mixed somehing up. {{ object | obj }} was looking at you.'
             ' You.'
@@ -47,23 +47,28 @@ class Rabbit(Object):
             ' anymore.'
         ])
         self.actions['look'] = self.look
-        
-        
+
+        self.speak_replies = Reply([
+            'You {{ verb | past }} {{ object | obj }}. You heared silence.'
+            ' What did you expect?'
+        ])
+        self.actions['speak'] = self.speak_replies
+
     def name(self):
         return self.named
-    
+
     def short_name(self):
         return 'rabbit'
-        
+
     def proper_name(self):
         return True
-        
+
     def look(self, data):
         if self.state['rabbit'] == 'hungry':
             self.look_hungry_replies.say(self.nar, data)
         else:
             self.look_fed_replies.say(self.nar, data)
-        
+
 
 if __name__ == '__main__':
 
@@ -76,28 +81,28 @@ if __name__ == '__main__':
     r.add_door(MetalDoor(nar, 'door002', pred=Predicate('right')))
     r.add_door(MetalDoor(nar, 'door003', pred=Predicate('front')))
     nar.enter(r)
-    
-    
+
     nar.interact('behold')
 #    nar.interact('look')
-#    
-#    nar.interact('behold left door')   
-#    nar.interact('behold left door')   
-#    nar.interact('behold left door')   
+#
+#    nar.interact('behold left door')
+#    nar.interact('behold left door')
+#    nar.interact('behold left door')
 #    nar.interact('behold left door')
 
     nar.interact('look rabbits')
-   
+
     nar.interact('look door')
     nar.interact('look rabbit')
     nar.interact('touch bowser')
     nar.interact('look bowser')
     nar.require_uid_state('bowser')['rabbit'] = 'fed'
     nar.interact('look bowser')
-    
+    nar.interact('speak bowser')
+
 #    nar.interact('speak left door')
-    
-    
+
+
 #    nar.interact('behold door')
 #    nar.interact('behold left door')
 #    nar.interact('behold right door')
